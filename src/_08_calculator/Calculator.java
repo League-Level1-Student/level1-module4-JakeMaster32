a6bdb88e6c1410a871de4d1aa968ebfbc03e87b5
League Student <league@iMac-29.attlocal.net>
2019-12-09 18:28:42 -0800

    calculator, pig latin IP
    YEEEEEEEEEE

--- src/_08_calculator/Calculator.java
+++ src/_08_calculator/Calculator.java
@@ -1,16 +1,17 @@
 package _08_calculator;
 
 public class Calculator {
-static void add(int x, int y) {
-	int z = x+y;
+
+static int add(int x, int y) {
+	return x+y;
 }
-static void subtract(int x, int y) {
-	int z = x-y;
+static int subtract(int x, int y) {
+	return x-y;
 }
-static void multiply(int x, int y) {
-	int z = x*y;
+static int multiply(int x, int y) {
+	return x*y;
 }
-static void divide(int x, int y) {
-	int z = x/y;
+static int divide(int x, int y) {
+	return x/y;
 }
 }
--- src/_08_calculator/Calculator_runner.java
+++ src/_08_calculator/Calculator_runner.java
@@ -1,6 +1,8 @@
 package _08_calculator;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,17 +10,29 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator_runner {
+public class Calculator_runner implements ActionListener {
+	
+	JButton add = new JButton("+");
+	JButton subtract = new JButton("-");
+	JButton multiply = new JButton("x");
+	JButton divide = new JButton("/");
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField field1 = new JTextField(11);
+	JTextField field2 = new JTextField(11);
+	JLabel label = new JLabel("0");
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JButton add = new JButton("+");
-		JButton subtract = new JButton("-");
-		JButton multiply = new JButton("x");
-		JButton divide = new JButton("/");
-		JTextField field1 = new JTextField(11);
-		JTextField field2 = new JTextField(11);
-		JLabel label = new JLabel("0");
+		
+		Calculator_runner calculator = new Calculator_runner();
+		calculator.setup();
+	}
+
+void setup() {
+add.addActionListener(this);
+subtract.addActionListener(this);
+multiply.addActionListener(this);
+divide.addActionListener(this);
+		
 		label.setPreferredSize(new Dimension(40, 40));
 		frame.add(panel);
 		panel.add(field1);
@@ -31,4 +45,27 @@ public class Calculator_runner {
 		frame.setSize(350, 150);
 		frame.setVisible(true);
 	}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	int answer = 0;
+	String c = field1.getText();
+	int d = Integer.parseInt(c);
+	String g = field2.getText();
+	int f = Integer.parseInt(g);
+	if (e.getSource().equals(add)) {
+		answer = Calculator.add(d, f);
+	}
+	else if (e.getSource().equals(subtract)) {
+		answer = Calculator.subtract(d, f);
+	}
+	else if (e.getSource().equals(multiply)) {
+		answer = Calculator.multiply(d, f);
+	}
+	else {
+		answer = Calculator.divide(d, f);
+	}
+	label.setText(""+answer);
+}
 }
